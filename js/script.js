c51f39aa8899985d949f25e9ccff405f98529fa8
Jonathan Crecencio <jonathancrecencio@outlook.com>
2020-09-19 00:41:45 -0300

    Fetch criado para trazer as imagens do filme dinamicamente

--- js/script.js
+++ js/script.js
@@ -4,12 +4,64 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=e5078ac25311497ae421e0
 .then(response => response.json())
 .then(data => {
     data.results.map((i, j) => {
-        if(j > 10){
+        if(j > 15){
             return;
         }
 
         if(j ==0){
             main.innerHTML += `<div class="banner-principal" style="background-image:url('https://image.tmdb.org/t/p/original${i.backdrop_path}')"></div>`
+        }else{
+            if(j == 1){
+                main.innerHTML += `
+                <h2>Assista a seguir</h2>
+                <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500${i.poster_path}" /></div>
+
+                `
+            }else{
+                main.innerHTML += `
+                <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500${i.poster_path}" /></div>`
+
+            }
+        }
+    })
+})
+
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=e5078ac25311497ae421e00502e3954c&language=en-US&page=2')
+.then(response => response.json())
+.then(data => {
+    data.results.map((i, j) => {
+        if(j > 15){
+            return;
+        }
+
+        if(j ==0){
+            main.innerHTML += `<div class="banner-principal" style="margin-top: 20px;background-image:url('https://image.tmdb.org/t/p/original${i.backdrop_path}')"></div>`
+        }else{
+            if(j == 1){
+                main.innerHTML += `
+                <h2>Assista a seguir</h2>
+                <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500${i.poster_path}" /></div>
+
+                `
+            }else{
+                main.innerHTML += `
+                <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500${i.poster_path}" /></div>`
+
+            }
+        }
+    })
+})
+
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=e5078ac25311497ae421e00502e3954c&language=en-US&page=3')
+.then(response => response.json())
+.then(data => {
+    data.results.map((i, j) => {
+        if(j > 15){
+            return;
+        }
+
+        if(j ==0){
+            main.innerHTML += `<div class="banner-principal" style="margin-top: 20px;background-image:url('https://image.tmdb.org/t/p/original${i.backdrop_path}')"></div>`
         }else{
             if(j == 1){
                 main.innerHTML += `
